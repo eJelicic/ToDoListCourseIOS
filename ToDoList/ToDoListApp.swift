9617ff5571cca49b7c66260430b219e6a014cebc
eJelicic <eugen.jlc@gmail.com>
2023-12-29 15:07:15 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Eugen Jelicic on 29.12.2023..
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
